44a7ef9e949a48104572f2372ab2fe23d329f007
Md. Majharul Islam <majharul.islam03@northsouth.edu>
2025-03-13 00:39:00 +0600

    tuples

--- app.ts
+++ app.ts
@@ -1,2 +1,26 @@
+// Basic Type
+// Primitive Types (number, string, boolean)
+// and reference
+// array
+// tuples
+// enums
+// any unkwon void null undefine never
+//
 
-// primitive and reference
+
+// Primitive and Reference
+
+
+// Primitive
+let a = 23;
+let nameInfo = "majharul";
+let coming = true;
+
+
+//Arrays
+let arr = [1,2,3];
+let arr1 = [1,2,3,{name1: "Majharul"}, {age: 12}];
+
+
+//Tuples
+let arr2: [string, number] = ["majharul", 12];
